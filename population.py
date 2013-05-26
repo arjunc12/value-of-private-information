8696ea4c84556c534668d3f749cf1ff32efc8068
Bryan He <bryanhe@caltech.edu>
2013-05-26 01:21:22 -0700

    Added documentation.

--- population.py
+++ population.py
@@ -1,9 +1,28 @@
+"""
+This class contains the information for a group of different private types.
+Each private type appears with a certain probability, and each private type
+has a distribution for its cost function.
+"""
+
 import random
+
 class Population:
+  """
+  probability: list of floating point numbers that sum (approximately) to 1
+               represent the probability that each private type appears
+  distribution: list of Distributions
+                represent the distribution of cost functions
+  Note: The lengths of probability and distribution must be equal.
+  """
   def __init__(self, probability, distribution):
     self.probability = probability
     self.distribution = distribution
     self.num_types = len(probability)
+
+  """
+  Returns a 2-tuple that consists of the (1) private type and (2) value for
+  privacy of a random individual from the population.
+  """
   def sample(self):
     p = random.random()
     priv_type = -1
@@ -17,6 +36,12 @@ class Population:
     value = self.distribution[i].sample()
 
     return (priv_type, value)
+
+  """
+  Returns a string that represents the population.
+  The string consists of the distributions and probabilities of the different
+  private types
+  """
   def __str__(self):
     string = ""
     for i in range(self.num_types):
