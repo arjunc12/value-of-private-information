f2a7cc974310ead9be947277f2f249bdd42b177f
Eric Martin <e.a.martin1337@gmail.com>
2013-06-01 23:01:34 -0700

    got rid of some magic values

--- learner.py
+++ learner.py
@@ -2,15 +2,18 @@
 This class is the abstract definition for a learning mechanism.
 """
 
+FIRST_OFFER = -2
+OFFER_REJECTED = -1
+
 class Learner(object):
     """
     update will be called repeatedly by the main loop. Each call explains the
     result of the previous query and expects an offer for the next payout.
 
     priv_type: private type of the previous query
-                         -2: first query, no previous query to report
-                         -1: previous offer rejected
-                         0 - ???: private type of the previous query (offer accepted)
+                         FIRST_OFFER: first query, no previous query to report
+                         OFFER_REJECTED: previous offer rejected
+                         priv_type >= 0 : private type of the previous query (offer accepted)
 
     Returns an offer for the next payout.
     """
