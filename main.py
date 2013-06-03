9cbf28bfc7e481750c95543e2ec63b768d19c3e3
Arjun Chandrasekhar <achandra@caltech.edu>
2013-06-03 01:41:33 -0700

    added command line tags

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import random
+import sys
+import os
+from datatime import datetime
+from pickle import Pickler
 
 from basic_learner import BasicLearner
 from np_learner import NPLearner
@@ -24,11 +28,30 @@ def main():
 
     # Initialize a non-parametric learner
     learner = NPLearner(len(distribution), 15)
-
-    d = driver.Driver(distribution, driver.STEPS, 10000, learner)
-
+    
+    constraint_type = driver.STEPS
+    constraint_val = 1000
+    args = sys.argv
+    
+    if '-c' in args:
+        constraint_type = driver.COST
+        i = args.index('-c')
+        constraint_val = float(args[i + 1])
+    elif '-s' in args:
+        i = args.index('-s')
+        constraint_val = float(args[i + 1])
+     
+    d = driver.Driver(distribution, constraint_type, constraint_val, learner)
+    
     # Obtain the prediction population
     (prediction, payout, individuals) = d.run()
+    
+    if '--save' in args:
+        if not os.path.exists('data'):
+            os.makedirs('data')
+        f = open(str(datetime.now()) + '.dat', 'w')
+        p.dump(f, (distribution, constraint_type, constraint_val, learner, prediction, payout, individuals))
+    
 
     # Output information about the test
     print("population:\n" + str(d.population))
