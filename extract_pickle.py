ce51b3c29d142e97c7a560640421429161e21abd
Bryan He <bryanhe@caltech.edu>
2013-06-05 17:48:54 -0700

    Adding newlines for pdf.

--- extract_pickle.py
+++ extract_pickle.py
@@ -1,7 +1,7 @@
 import pickle
 
-time = "2013-06-05-04-56-06"
-offer_type = "5"
+time = "2013-06-05-17-38-05"
+offer_type = "max"
 f = open("data/run_" + time + "_" + offer_type, "r")
 (prediction, payout, individuals, divergences, costs) = pickle.load(f)
 print prediction
@@ -25,6 +25,6 @@ f1 = open("data/" + offer_type + "_pdf1", "w")
 f2 = open("data/" + offer_type + "_pdf2", "w")
 x = start
 while x <= end:
-    f1.write(str(prediction.distribution[0].pdf(x)))
-    f2.write(str(prediction.distribution[1].pdf(x)))
+    f1.write(str(prediction.distribution[0].pdf(x)) + "\n")
+    f2.write(str(prediction.distribution[1].pdf(x)) + "\n")
     x += inc
